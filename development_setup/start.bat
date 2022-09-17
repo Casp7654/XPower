9ed19ee358a776a1f7592a794215244736814dae
Casper Andersen <ndersorn@outlook.dk>
2022-09-17 19:05:57 +0200

    fixed Start.bat

--- development_setup/start.bat
+++ development_setup/start.bat
@@ -1,11 +1,6 @@
-docker run --rm -itd xpower-iothandler --mount type=bind,src="%cd%/sourcecode/handler",dst=/root/source/handler --name xpower-iothandler
-
-docker run --rm -itd xpower-webapi --mount type=bind,src="%cd%/volumes/webapi/backups",dst=/root/backups --mount type=bind,src="%cd%/sourcecode/webapi",dst=/root/source/webapi --name xpower-webapi
-
-docker run --rm -itd xpower-pwa --mount type=bind,src="$(pwd)/sourcecode/pwa",dst=/root/source/pwa --name xpower-pwa
-
-docker run --rm -itd xpower-hub --mount type=bind,src="$(pwd)/volumes/hub/backups",dst=/root/backups --mount type=bind,src="$(pwd)/sourcecode/hub",dst=/root/source/hub --name xpower-hub1
-
-docker run --rm -itd xpower-iot --mount type=bind,src="$(pwd)/volumes/iot/backups",dst=/root/backups --mount type=bind,src="$(pwd)/sourcecode/iot",dst=/root/source/iot --name xpower-iot1
-
-docker run --rm -itd xpower-iot --mount type=bind,src="$(pwd)/volumes/iot/backups",dst=/root/backups --mount type=bind,src="$(pwd)/sourcecode/iot",dst=/root/source/iot --name xpower-iot2
+docker run --rm -itd --name xpower-iot_handler --mount type=bind,src="%cd%/sourcecode/handler",dst=/root/source/handler xpower-iot_handler
+docker run --rm -itd --name xpower-webapi --mount type=bind,src="%cd%/volumes/webapi/backups",dst=/root/backups --mount type=bind,src="%cd%/sourcecode/webapi",dst=/root/source/webapi xpower-webapi
+docker run --rm -itd --name xpower-pwa --mount type=bind,src="$(pwd)/sourcecode/pwa",dst=/root/source/pwa xpower-pwa
+docker run --rm -itd --name xpower-hub1 --mount type=bind,src="$(pwd)/volumes/hub/backups",dst=/root/backups --mount type=bind,src="$(pwd)/sourcecode/hub",dst=/root/source/hub xpower-hub
+docker run --rm -itd --name xpower-iot1 --mount type=bind,src="$(pwd)/volumes/iot/backups",dst=/root/backups --mount type=bind,src="$(pwd)/sourcecode/iot",dst=/root/source/iot xpower-iot
+docker run --rm -itd --name xpower-iot2 --mount type=bind,src="$(pwd)/volumes/iot/backups",dst=/root/backups --mount type=bind,src="$(pwd)/sourcecode/iot",dst=/root/source/iot xpower-iot
