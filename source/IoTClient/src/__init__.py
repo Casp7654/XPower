4164b4961de2d3f2b34ca328f8be950914e6ac72
Emil Andersen <emil.rauff.andersen@hotmail.com>
2022-09-17 08:07:29 +0200

    Basic mqtt

--- source/IoTClient/src/__init__.py
+++ source/IoTClient/src/__init__.py
@@ -1,8 +1,32 @@
-import managers.grpc_manager as grpc
+import paho.mqtt.client as mqtt
 
+server_ip = "broker.emqx.io"
+server_port = 1883
+server_keep_alive = 60
+mqtt_client = mqtt.Client()
+
+## Client
+def on_Connect(client, userdata, flags, rc):
+    print("Connected")
+
+    client.subscribe("socket")
+
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+def start_client(client):
+    client.on_connect = on_Connect
+    client.on_message = on_message
+
+    client.connect(server_ip, server_port, server_keep_alive)
+
+    client.loop_forever()
+
+
+## Main
 def main():
-    gManager = grpc.grpcmanager()
-    gManager.hello()
+    start_client(mqtt_client)
+    
 
 
 if __name__ == "__main__":
