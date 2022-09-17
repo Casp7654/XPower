5c723c51060296c2604bc54f4387a528b3130138
Casper Andersen <ndersorn@outlook.dk>
2022-09-17 18:55:41 +0200

    Changed syntax for build.bat

--- development_setup/build.bat
+++ development_setup/build.bat
@@ -3,20 +3,20 @@ mkdir resource\volumes\hub\backups
 mkdir resource\volumes\iot\backups
 
 echo "Building WebApi"
-docker build . --tag xpower-webapi --file ".\dockerfiles\webapi.Dockerfile"
+docker build . --tag xpower-webapi --file "%cd%\resource\dockerfiles\webapi.Dockerfile"
 
 echo "Building IoT Handler"
-docker build . --tag xpower-iot_handler --file ".\dockerfiles\iot_handler.Dockerfile"
+docker build . --tag xpower-iot_handler --file "%cd%\resource\dockerfiles\iot_handler.Dockerfile"
 
 echo "Building Hosted PWA"
-docker build . --tag xpower-hosted_pwa --file ".\dockerfiles\hosted_pwa.Dockerfile"
+docker build . --tag xpower-hosted_pwa --file "%cd%\resource\dockerfiles\hosted_pwa.Dockerfile"
 
 echo "Building Hub"
-docker build . --tag xpower-hub --file ".\dockerfiles\hub.Dockerfile"
+docker build . --tag xpower-hub --file "%cd%\resource\dockerfiles\hub.Dockerfile"
 
 echo "Building IoT Device"
-docker build . --tag xpower-iot --file ".\dockerfiles\iot.Dockerfile"
+docker build . --tag xpower-iot --file "%cd%\resource\dockerfiles\iot.Dockerfile"
 
 wsl --exec "docker rmi $(docker images --format '{{.ID\t{{.Tag}}' | grep '<none>' | awk '{print $1}')"
-docker builder prune --force
-docker image prune --force
+docker builder prune
+docker image prune
