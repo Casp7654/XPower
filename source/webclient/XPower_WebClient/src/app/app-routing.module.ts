6ec70f95a763cfa9a0860848d70fb201cf0a1118
Rene Lorentzen <rene@unifactory.dk>
2022-09-21 12:55:24 +0200

    added comment

--- source/webclient/XPower_WebClient/src/app/app-routing.module.ts
+++ source/webclient/XPower_WebClient/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HubControllerComponent } from './Hub/hub-controller/hub-controller.component';
 
+//Path definitions for components that is used by routing
 const routes: Routes = [
   { path: '', component: HubControllerComponent}
 ]
