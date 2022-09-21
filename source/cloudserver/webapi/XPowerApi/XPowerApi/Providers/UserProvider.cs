f192b30c85948b70da0bfa1ea031b9edd560e135
Casper Andersen <ndersorn@outlook.dk>
2022-09-21 12:57:44 +0200

    Update in setup so it works on linux

--- source/cloudserver/webapi/XPowerApi/XPowerApi/Providers/UserProvider.cs
+++ source/cloudserver/webapi/XPowerApi/XPowerApi/Providers/UserProvider.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Net.Http;
+﻿using System.Net.Http.Headers;
 using XPowerApi.Interfaces;
 using XPowerApi.Models.UserModels;
 
@@ -7,53 +6,51 @@ namespace XPowerApi.Providers
 {
     public class UserProvider : IUserProvider
     {
-        private IConfiguration _configuration;
         private HttpClient _httpClient;
-        private List<User> users = new List<User>();
+        private List<User> _users = new List<User>();
 
         public UserProvider(IConfiguration configuration)
         {
-            _configuration = configuration;
             _httpClient = new HttpClient();
-            _httpClient.BaseAddress = new Uri(_configuration["SurrealDB"]["ConnStr"]);
-            _httpClient.DefaultRequestHeaders.Accept.Clear();
+            _httpClient.BaseAddress = new Uri(configuration["SurrealDBConnStr"]);
             _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            _httpClient.DefaultRequestHeaders.User.Add("root:root");
-            _httpClient.DefaultRequestHeaders.Add("NS: xpower");
-            _httpClient.DefaultRequestHeaders.Add("DB: webapi");
+            _httpClient.DefaultRequestHeaders.Authorization = AuthenticationHeaderValue.Parse("root:root");
+            _httpClient.DefaultRequestHeaders.Add("NS:","xpower");
+            _httpClient.DefaultRequestHeaders.Add("DB:","webapi");
         }
 
         public Task<User> CreateUser(UserCreate userCreate)
         {
             var createdUser = new User() { Id = Random.Shared.Next(4, 150), UserName = userCreate.UserName };
-            users.Add(createdUser);
+            _users.Add(createdUser);
 
             return Task.Run(() => createdUser);
         }
 
         public Task<bool> DeleteUser(int id)
         {
-            var user = users.Find(x => x.Id == id);
+            var user = _users.Find(x => x.Id == id);
 
             if (user == null)
                 return Task.Run(() => false);
 
-            return Task.Run(() => users.Remove(user));
+            return Task.Run(() => _users.Remove(user));
         }
 
         public async Task<User> GetUserByID(int id)
         {
             HttpRequestMessage request = new HttpRequestMessage();
-            request.Content = "select * from user";
-            HttpResponseMessage response = await _httpClient.SendAs(request);
+            request.Method = HttpMethod.Post;
+            request.Content = new StringContent("select * from user");
+            HttpResponseMessage response = await _httpClient.SendAsync(request);
             Console.WriteLine(response.Content.ToString());
 
-            return Task.Run(() => users.Find(x => x.Id == id));
+            return await Task.Run(() => _users.Find(x => x.Id == id));
         }
 
         public Task<User> UpdateUserUsername(int id, string username)
         {
-            var user = users.Find(x => x.Id == id);
+            var user = _users.Find(x => x.Id == id);
 
             if (user == null)
                 return Task.Run(() => user); // because i cant return null :/
