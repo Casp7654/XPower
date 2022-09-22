af65058aab90fd431ab4c55b17ed51581dc02e4d
Rene Lorentzen <rene@unifactory.dk>
2022-09-22 14:10:03 +0200

    added missing files

---
+++ source/webclient/XPower_WebClient/src/app/IoT/iot-controller/iot-controller.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-iot-controller',
+  templateUrl: './iot-controller.component.html',
+  styleUrls: ['./iot-controller.component.scss']
+})
+export class IotControllerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
