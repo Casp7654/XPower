781f6085b57bf8df62df8e78df929d2bc31ae01e
EADR (Emil Andersen) <EADR@nnit.com>
2022-09-22 10:12:47 +0200

    Added xunit test

---
+++ source/hub/tests/ConnectionHandler/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace ConnectionHandler;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.Equal(true, true);
+    }
+}
