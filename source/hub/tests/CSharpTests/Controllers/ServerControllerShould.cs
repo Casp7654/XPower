530a3d5aa9e8859d4255a121d0b2ce3c5d6a037b
EADR (Emil Andersen) <EADR@nnit.com>
2022-09-22 10:33:46 +0200

    Renamed project

---
+++ source/hub/tests/CSharpTests/Controllers/ServerControllerShould.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+public class ServerControllerShould {
+    [Fact]
+    void CallMessageReceived_WhenServerHandlerReceivesMessage() {
+        // Arrange
+        
+        // Act
+
+        // Assert
+        Assert.Equal(1, 1);
+    }
+}
