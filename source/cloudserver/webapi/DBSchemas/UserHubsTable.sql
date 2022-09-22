fc057398866390dcce930cde57d6e180109f84ab
Casper Andersen <ndersorn@outlook.dk>
2022-09-22 12:51:09 +0200

    updated database setup

--- source/cloudserver/webapi/DBSchemas/UserHubsTable.sql
+++ source/cloudserver/webapi/DBSchemas/UserHubsTable.sql
@@ -2,8 +2,9 @@
 define table userhubs Schemafull;
 
 -- Set Fields in table
+define field uid on table user type string;
 define field user_id on table hub type int;
 define field hub_id on table hub type int;
 
 -- Set Unique ID
-define index uid on table hub columns user_id, hub_id unique;
+define index uid on table hub columns uid unique;
