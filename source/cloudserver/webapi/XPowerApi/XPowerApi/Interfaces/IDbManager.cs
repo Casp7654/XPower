5e5535ab2e1d2b3415b2a8d076a9deabc13c06fd
Casper Andersen <ndersorn@outlook.dk>
2022-09-23 11:22:32 +0200

    GetUserById And CreateUser Works

--- source/cloudserver/webapi/XPowerApi/XPowerApi/Interfaces/IDbManager.cs
+++ source/cloudserver/webapi/XPowerApi/XPowerApi/Interfaces/IDbManager.cs
@@ -2,15 +2,15 @@ namespace XPowerApi.Interfaces;
 
 public interface IDbManager
 {
-    public T Create<T>(string tableName, T newT);
+    public Task<T> Create<T>(string tableName, Dictionary<string, string> dataArray) where T : new();
     
-    public int GetNextId(string tableName);
-    public T GetOne<T>(string tableName, int? id, string whereClause = "");
-    public T GetSpecific<T>(string tableName, string whereClause = "");
-    public T GetAll<T>(string tableName);
+    public Task<int> GetNextId(string tableName);
+    public Task<T> GetOneById<T>(string tableName, int id);
+    public Task<T> GetSpecific<T>(string tableName, string whereClause = "");
+    public Task<T> GetAll<T>(string tableName);
 
-    public T Update<T>(string tableName, int id,  T newT);
+    public Task<T> Update<T>(string tableName, int id,  T newT);
     
-    public T Delete<T>(string tableName, T oldT);
+    public Task<T> Delete<T>(string tableName, T oldT);
         
 }
