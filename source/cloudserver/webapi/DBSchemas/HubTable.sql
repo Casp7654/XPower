549ea5cff3f1f70af8bebe579edfa1de9761cb2a
Casper Andersen <ndersorn@outlook.dk>
2022-09-23 11:22:04 +0200

    updated Rest.MD

--- source/cloudserver/webapi/DBSchemas/HubTable.sql
+++ source/cloudserver/webapi/DBSchemas/HubTable.sql
@@ -3,5 +3,6 @@ define table hub Schemafull;
 
 -- Set Fields in table
 define field name on table hub type string;
+DEFINE FIELD mac ON hub TYPE string;
 define field private_addr on table hub type string;
 define field public_addr on table hub type string;
