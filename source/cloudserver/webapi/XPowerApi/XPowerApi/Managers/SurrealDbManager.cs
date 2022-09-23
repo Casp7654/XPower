5984f67dea56a54828a6e5752b7f92905157ebb8
Casper Andersen <ndersorn@outlook.dk>
2022-09-23 11:22:32 +0200

    GetUserById And CreateUser Works

--- source/cloudserver/webapi/XPowerApi/XPowerApi/Managers/SurrealDbManager.cs
+++ source/cloudserver/webapi/XPowerApi/XPowerApi/Managers/SurrealDbManager.cs
@@ -1,6 +1,101 @@
+using System.Text.Json;
+using XPowerApi.Interfaces;
+using XPowerApi.DbModels.SurrealDbModels;
+
 namespace XPowerApi.Managers;
 
-public class SurrealDbManager
+public class SurrealDbManager : IDbManager
 {
-    
+    private SurrealDbHttpClient _httpClient;
+
+    public SurrealDbManager(IConfiguration configuration)
+    {
+        _httpClient = new SurrealDbHttpClient(configuration);
+    }
+
+    private async Task<SurrealDbResult> MakeRawResult(string sqlString)
+    {
+        // Create RequestMessage
+        SurrealDbHttpRequestMessage request = new SurrealDbHttpRequestMessage(sqlString);
+        // Get Response
+        HttpResponseMessage response = await _httpClient.SendAsync(request);
+        string jsonData = await response.Content.ReadAsStringAsync();
+        // Debug Raw Response 
+        //Console.WriteLine(responseJson);
+        // ERR
+        if (!response.IsSuccessStatusCode)
+            throw new Exception("Could not get data");
+        // UnPack Result
+        List<SurrealDbResult> dbResult = JsonSerializer.Deserialize<List<SurrealDbResult>>(jsonData)!;
+
+        return dbResult[0];
+    }
+
+    public async Task<T> Create<T>(string tableName, Dictionary<string, string> dataArray) where T : new()
+    {
+        // TODO: Fix Db Create
+        // Get Next id in table
+        int newId = await GetNextId(tableName);
+        dataArray.Add("id",$"{newId}");
+        // Set Base SQL
+        string sqlString = $"insert into {tableName} " + "{";
+        // loop data array to add params
+        foreach (KeyValuePair<string, string> dataIndex in dataArray)
+        {
+            sqlString += $"{dataIndex.Key}:";
+            sqlString += (int.TryParse(dataIndex.Value,out int irrelevant)) ? $"{dataIndex.Value}," : $"\"{dataIndex.Value}\"," ;
+        }
+        // trim last ,
+        sqlString = sqlString.TrimEnd(',') + "};";
+        // Make Request
+        SurrealDbResult dbResult = await MakeRawResult(sqlString);
+        // Return Created DB Object
+        T t =(dbResult.status == "OK") ?await GetOneById<T>(tableName, newId) : new T();
+        return t;
+    }
+
+    public async Task<int> GetNextId(string tableName)
+    {
+        int id = 0;
+        // Set SQL string
+        string sqlString = $"select id from {tableName} order by id desc limit 1;";
+        SurrealDbResult dbResult = await MakeRawResult(sqlString);
+        if (dbResult.result.Count >= 1)
+        {
+            // Spaghetti
+            IdObject t = JsonSerializer.Deserialize<IdObject>(JsonSerializer.Serialize(dbResult.result[0]))!;
+            id = int.Parse(t.id.Split(':')[1]);
+            id = (id != 0) ? id + 1 : 1;
+        }
+
+        return id;
+    }
+
+    public async Task<T> GetOneById<T>(string tableName, int id)
+    {
+        string sqlString = $"select * from {tableName} where id = {tableName}:{id} limit 1;";
+        SurrealDbResult dbResult = await MakeRawResult(sqlString);
+        T t = JsonSerializer.Deserialize<T>(JsonSerializer.Serialize(dbResult.result[0]))!;
+        return t;
+    }
+
+    public async Task<T> GetSpecific<T>(string tableName, string whereClause = "")
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<T> GetAll<T>(string tableName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<T> Update<T>(string tableName, int id, T newT)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<T> Delete<T>(string tableName, T oldT)
+    {
+        throw new NotImplementedException();
+    }
 }
--- source/cloudserver/webapi/XPowerApi/XPowerApi/Providers/UserProvider.cs
+++ source/cloudserver/webapi/XPowerApi/XPowerApi/Providers/UserProvider.cs
@@ -1,89 +1,50 @@
 ﻿using XPowerApi.Interfaces;
 using XPowerApi.DbModels;
 using XPowerApi.DbModels.SurrealDbModels;
+using XPowerApi.Managers;
 using XPowerApi.Models.UserModels;
 
 namespace XPowerApi.Providers
 {
     public class UserProvider : IUserProvider
     {
-        private SurrealDbHttpClient _httpClient;
-        private List<User> _users = new List<User>();
+        private SurrealDbManager _dbManager;
 
         public UserProvider(IConfiguration configuration)
         {
-            _httpClient = new SurrealDbHttpClient(configuration);
-        }
-
-        public async Task<int> GetNextUserId()
-        {
-            // Set SQL string
-            string sqlString = $"select id from user order by id desc limit 1;";
-            // Create RequestMessage
-            SurrealDbHttpRequestMessage request = new SurrealDbHttpRequestMessage(sqlString);
-            // Get Response
-            HttpResponseMessage response = await _httpClient.SendAsync(request);
-            string responseJson = await response.Content.ReadAsStringAsync();
-            // Debug response
-            //Console.WriteLine(responseJson);
-            // ERR
-            if (!response.IsSuccessStatusCode)
-                throw new Exception("Could not get data");
-
-            // Create Object
-            int id = SurrealDbResultFactory.GetIdFromResult(responseJson);
-            return (id != 0) ? id + 1 : -1;
+            _dbManager = new SurrealDbManager(configuration);
         }
 
         public async Task<User> CreateUser(UserCreate userCreate)
         {
-            int id = await GetNextUserId();
-            string sqlString =
-                $"insert into user {{id:{id},username:\"{userCreate.UserName}\",hashed_password:\"{userCreate.Password}\"}}";
-
-            return new User();
+            // Generate Salt
+            string salt = "hestepest"; // TODO: Make Salt
+            // Create DB Object
+            Dictionary<string, string> dataArray = new Dictionary<string, string>()
+            {
+                {"hashed_password", userCreate.Password},
+                {"username",userCreate.UserName},
+                {"salt",salt}
+            };
+            // Create Dbobject and Convert to User on success
+            User user = (await _dbManager.Create<UserDb>("user", dataArray)).ConvertToUser();
+            return user;
         }
 
         public Task<bool> DeleteUser(int id)
         {
-            var user = _users.Find(x => x.Id == id);
-
-            if (user == null)
-                return Task.Run(() => false);
-
-            return Task.Run(() => _users.Remove(user));
+            throw new NotImplementedException();
         }
 
         public async Task<User> GetUserById(int id)
         {
-            // Set SQL string
-            string sqlString = $"select * from user where id = user:{id};";
-            // Create RequestMessage
-            SurrealDbHttpRequestMessage request = new SurrealDbHttpRequestMessage(sqlString);
-            // Get Response
-            HttpResponseMessage response = await _httpClient.SendAsync(request);
-            string responseJson = await response.Content.ReadAsStringAsync();
-            // Debug response
-            //Console.WriteLine(responseJson);
-            // ERR
-            if (!response.IsSuccessStatusCode)
-                throw new Exception("Could not get data");
-
-            // Create Object
-            User user = SurrealDbResultFactory.MakeOne<UserDb>(responseJson).ConvertToUser();
+            User user = (await _dbManager.GetOneById<UserDb>("user", id)).ConvertToUser();
             return user;
         }
 
         public Task<User> UpdateUserUsername(int id, string username)
         {
-            var user = _users.Find(x => x.Id == id);
-
-            if (user == null)
-                return Task.Run(() => user); // because i cant return null :/
-
-            user.UserName = username;
-
-            return Task.Run(() => user);
+            throw new NotImplementedException();
         }
     }
 }
