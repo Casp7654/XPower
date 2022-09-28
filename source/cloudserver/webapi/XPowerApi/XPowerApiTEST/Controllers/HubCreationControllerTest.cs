6401629ee5bc74b0a6aedaaf303ffa5e091b7361
Casper Andersen <ndersorn@outlook.dk>
2022-09-28 12:14:19 +0200

    updated UserControllerTests

---
+++ source/cloudserver/webapi/XPowerApi/XPowerApiTEST/Controllers/HubCreationControllerTest.cs
@@ -0,0 +1,19 @@
+using XPowerApi.Controllers;
+using XPowerApi.Interfaces;
+using XPowerApi.Models.UserModels;
+using ILogger = Castle.Core.Logging.ILogger;
+
+namespace XPowerApiTEST.Controllers
+{
+    public class HubCreationController
+    {
+        private readonly HubCreationController _subject;
+        private readonly Mock<ILogger<HubCreationController>> _logger = new();
+        private readonly Mock<IHubManager> _hubManager = new();
+        
+        public HubCreationControllerTest
+        
+        
+    }
+    
+}
